ac7c7d3d5951117719b8329fb4d20915f849bda2
Daniel Micay <daniel.micay@grapheneos.org>
2024-07-08 21:55:38 -0400

    update build tools to 35.0.0

--- config-holder/app/build.gradle.kts
+++ config-holder/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     namespace = "app.grapheneos.gmscompat.config"
 
     compileSdk = 34
-    buildToolsVersion = "34.0.0"
+    buildToolsVersion = "35.0.0"
 
     defaultConfig {
         minSdk = 32
