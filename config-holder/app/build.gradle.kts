b9672a130525f748e1f62ea0cb3ba87e34ec46be
Daniel Micay <daniel.micay@grapheneos.org>
2024-10-16 20:16:31 -0400

    config-holder: increment version to 143

--- config-holder/app/build.gradle.kts
+++ config-holder/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     defaultConfig {
         minSdk = 32
         targetSdk = 35
-        versionCode = 142
+        versionCode = 143
         versionName = versionCode.toString()
     }
 
